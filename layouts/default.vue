be8130122c1796d0fa8af90f8220b3c334fac4b1
Brice Johnson <bricej13@gmail.com>
2021-10-27 22:14:17 -0600

    Fixed bug with vuex-persist & axios

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,35 +1,34 @@
 <template>
-  <div>
-    <b-navbar type="is-primary is-fixed-top">
-      <template #brand>
-        <b-navbar-item tag="router-link" :to="{ path: '/' }">
-          <span class="is-size-4 has-text-weight-bold">Thunderdrome</span>
-        </b-navbar-item>
-      </template>
+  <div class="default-layout">
+    <div class="Header">
+      <b-navbar type="is-primary">
+        <template #brand>
+          <b-navbar-item tag="router-link" :to="{ path: '/' }">
+            <span class="is-size-4 has-text-weight-bold">Thunderdrome</span>
+          </b-navbar-item>
+        </template>
 
-      <template #end>
-        <b-navbar-item tag="div">
-          <div class="buttons">
-            <a class="button is-primary" @click.prevent="logout">
-              <strong>Log out {{ $store.state.user.name }}</strong>
-            </a>
-          </div>
-        </b-navbar-item>
-      </template>
-    </b-navbar>
-
-    <section class="columns">
-      <div class="column is-2">
-        <Sidebar />
-      </div>
-      <div class="container column is-10">
-        <Nuxt />
-      </div>
-    </section>
-    <nav class="navbar is-fixed-bottom is-transparent">
+        <template #end>
+          <b-navbar-item tag="div">
+            <div class="buttons">
+              <a class="button is-primary" @click.prevent="logout">
+                <strong>Log out {{ $store.state.user.name }}</strong>
+              </a>
+            </div>
+          </b-navbar-item>
+        </template>
+      </b-navbar>
+    </div>
+    <div class="Menu">
+      <Sidebar />
+    </div>
+    <div class="Content">
+      <Nuxt />
+    </div>
+    <div class="Play-Queue" />
+    <div class="Player">
       <audio-player />
-      <audio :src="$store.state.player.playlist[0]" />
-    </nav>
+    </div>
   </div>
 </template>
 
@@ -45,7 +44,7 @@ export default {
   head () {
     return {
       htmlAttrs: {
-        class: ['has-navbar-fixed-top', 'has-navbar-fixed-bottom']
+        class: []
       }
     }
   },
@@ -57,3 +56,28 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.default-layout {
+  height: 100vh;
+  display: grid;
+  grid-template-columns: 1fr 4fr 1fr;
+  grid-template-rows: auto 1fr auto;
+  gap: 0px 5px;
+  grid-auto-flow: row;
+  grid-template-areas:
+    "Header Header Header"
+    "Menu Content Play-Queue"
+    "Player Player Player";
+}
+
+.Header { grid-area: Header; }
+
+.Menu { grid-area: Menu; }
+
+.Play-Queue { grid-area: Play-Queue; }
+
+.Content { grid-area: Content; overflow-y: auto; }
+
+.Player { grid-area: Player; }
+</style>
--- plugins/vuex-persist.js
+++ plugins/vuex-persist.js
@@ -1,10 +1,21 @@
 import { VuexPersistence } from 'vuex-persist'
 
-export default function ({ store }) {
-  new VuexPersistence({
+export default function ({ store, $axios }) {
+  const vp = new VuexPersistence({
     key: 'thunderdrome',
     storage: window.localStorage,
-    modules: ['user']
-    // filter: mutation => mutation.type === 'login' || mutation.type === 'logout'
-  }).plugin(store)
+    modules: ['user'],
+    restoreState: (key, storage) => {
+      const state = JSON.parse(localStorage.getItem(key))
+      $axios.setBaseURL(state.user.baseUrl)
+      return state
+    },
+    saveState: (key, state, storage) => localStorage.setItem(key, JSON.stringify(state))
+
+    // filter: (mutation) => {
+    //   console.log(mutation)
+    //   return mutation.type === 'user/login' || mutation.type === 'user/logout'
+    // }
+  })
+  vp.plugin(store)
 }
